6cc4096d5cd566d8f2d5af0795b31d1dcecb5f87
Hannah Hazi <hannah.hazi@paconsulting.com>
2017-01-11 16:04:50 +0000

    Tweaks, spotted bug, trying to fix in a nice way

--- game_of_snake.py
+++ game_of_snake.py
@@ -4,7 +4,6 @@ from random import randint
 
 sense = SenseHat()
 
-
 x = 0
 y = 0
 length = 4
@@ -14,41 +13,48 @@ def set_all(r, g, b):
     for i in range(0,8):
         for j in range(0,8):
             sense.set_pixel(i, j, r, g, b)
-            
+
+
 def restart():
     global x, y, length, tail
     x, y = 0, 0
     length = 4
     tail = [(0,0)]
-    sense.set_pixel(x, y, 255, 0, 0)
-    sense.set_pixel(randint(0, 7), randint(0, 7), 0, 255, 0) # pixel to eat
+    sense.set_pixel(x, y, 255, 0, 0)  # snake starts in the top left corner
+    a_wild_pixel_appears()  # pixel to eat
 
-    
 
-def you_fail():
+def fail_animation():
     for i in range(0, 10):
         set_all(0, 0, 255)
         sleep(0.1)
-        set_all(0, 0, 0)
+        sense.clear()
         sleep(0.1)
-    
 
-set_all(0, 0, 0)
+
+def a_wild_pixel_appears():
+    x_wild = randint(0,7)
+    y_wild = randint(0,7)
+    # we need to exclude pixel locations that are already occupied, in a general way...
+
+    sense.set_pixel(x_wild, y_wild, 0, 255, 0)
+
+sense.clear()
 restart()
 
-test = sense.get_pixels()
-print(test)
+board_state = sense.get_pixels()
+print(board_state)
 
 
 while True:
-    event = sense.stick.wait_for_event()
-            
+    event = sense.stick.wait_for_event()  #take joystick input
+
+    # modulus allows wrap around if fall off edge of 8*8 board
     if event.direction == "right":
         x = (x+1) % 8
     elif event.direction == "left":
         x = (x+7) % 8
     elif event.direction == "down":
-
         y = (y+1) % 8
     elif event.direction == "up":
         y = (y+7) % 8
@@ -62,9 +68,10 @@ while True:
     eaten_pixel = sense.get_pixels()[index_eaten]
     if eaten_pixel[1] != 0:
         length +=1
-        sense.set_pixel(randint(0, 7), randint(0, 7), 0, 255, 0) # pixel to eat
+        sense.set_pixel(randint(0, 7), randint(0, 7), 0, 255, 0) # new pixel to eat
+        # of course there's an interesting bug here: what if it's in our tail?
     elif eaten_pixel[0] != 0:
-        you_fail()
+        fail_animation()
         restart()
         continue
     
